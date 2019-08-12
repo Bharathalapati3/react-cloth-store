8e203e3bbb05090772955e8c38abdda421a67f59
Bharathalapati3 <bharathalapati03@gmail.com>
2019-08-12 22:18:52 +0530

    withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
 
 function App() {
+
+  const HatsPage= () =>(
+    <div><h1>Hats Pages</h1></div>
+  )
+  
   return (
     <div>
-     <HomePage />
+     <Route exact path='/' component={HomePage} />
+     <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
